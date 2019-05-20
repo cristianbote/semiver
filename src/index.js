bdb595c0eb180f7111c3441594aecbe9aee51326
cristianbote <botecristian@yahoo.com>
2019-05-20 11:57:07 +0300

    Add caching for array creation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 var fn = new Intl.Collator(0, { numeric:1 }).compare;
+var map = {};
 
 export default function (a, b) {
-	a = a.split('.');
-	b = b.split('.');
+	a = map[a] || (map[a] = a.split('.'));
+	b = map[b] || (map[b] = b.split('.'));
 
 	return fn(a[0], b[0])
 			|| fn(a[1], b[1])
